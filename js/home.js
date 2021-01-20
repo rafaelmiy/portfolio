78f18a52f76ce0d503892599c0d4f8bf534c0738
Miyamoto <rafaelmiy@gmail.com>
2021-01-20 00:31:05 -0300

    up

---
+++ js/home.js
@@ -0,0 +1,18 @@
+// $('.nav-top a, .scrollDown').click(function() {
+//   $('html, body').animate({
+//     scrollTop: $($(this).attr('href'))
+//       .offset().top
+//   }, 700);
+//   return false;
+// });
+
+$(window).scroll(function() {
+  var x = $(".site-header").offset().top;
+  $(".grid").each(function(index) {
+    var z = $(this).attr("id");
+    if (x > $(this).offset().top && x <= $(this).offset().top + $(this).height()) {
+        $('.site-header a').removeClass("active");
+        $('a.' + z).addClass("active");
+    } 
+  })
+})
